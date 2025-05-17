54af84dc8f5d7741900b1f44e6b4799fce521592
abhi7893 <radha_thequeen@outlook.com>
2025-05-17 20:31:23 +0530

    reverse string

---
+++ L08/L08 revers a string.py
@@ -0,0 +1,13 @@
+
+string = input("Please enter you own String : ")
+
+reverse = ('')
+
+
+for i in string:
+    reverse = i + reverse
+
+print()
+
+print("The Original String = ", string)
+print("The Reversed String = ", reverse)
