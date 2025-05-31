fd8649f8c94e6aed465d073d482c42232c2e02a9
abhi7893 <radha_thequeen@outlook.com>
2025-05-31 20:46:56 +0530

    star

---
+++ L12/L12 star.py
@@ -0,0 +1,36 @@
+import turtle
+
+paper = turtle.Screen()
+paper.bgcolor("Aqua")
+paper.setup(500,500)
+paper.title("Star of Davidd")
+
+pen = turtle.Turtle()
+
+
+pen.pendown()
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+
+pen.penup()
+pen.right(150)
+pen.forward(50)
+
+
+pen.pendown()
+pen.right(90)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+
+turtle.done()
